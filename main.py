521616b74e5fca0709a635d3225a581c1c403c29
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-12-28 02:18:22 -0800

    rm uneeded blank lines/small refactor

--- main.py
+++ main.py
@@ -14,31 +14,31 @@ CLIENT_ID = os.environ.get('SPOTIPY_CLIENT_ID')
 CLIENT_SECRET = os.environ.get('SPOTIPY_CLIENT_SECRET')
 SPOTIPY_REDIRECT_URI = os.environ.get('SPOTIPY_REDIRECT_URL')
 
-
 user_date = input(
     "What date would you like the top 100 songs for? (YYYY-MM-DD): ")
 response = requests.get("https://billboard.com/charts/hot-100/" + user_date)
 soup = BeautifulSoup(response.text, "html.parser")
+
 artist_list = [title.getText().strip("\n").replace(" Featuring", ",").replace(" &", ",")
                for title in soup.select("span.c-label.a-no-trucate")]
 titles_list = [title.getText().strip("\n")
                for title in soup.select("li h3")][:100]
 
-scope = "playlist-modify-private"
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-    scope=scope,
-    redirect_uri=SPOTIPY_REDIRECT_URI,
-    client_id=CLIENT_ID,
-    client_secret=CLIENT_SECRET,
-    show_dialog=True,
-    cache_path="token.txt"
-))
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri=SPOTIPY_REDIRECT_URI,
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
 
 song_urls = []
-for i in range(0, 100):
+for i in range(100):
     track_info = sp.search(
-        q=f"{titles_list[i]} {artist_list[i]}",
-        type="track")
+        q=f"{titles_list[i]} {artist_list[i]}", type="track")
     try:
         url = track_info["tracks"]["items"][0]["uri"].split(":")[2]
         song_urls.append(url)
