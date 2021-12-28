3187591704aff3e3ac23dde718e221dbe3d72006
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-12-28 02:06:07 -0800

    add playlist creation funtionality

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from bs4 import BeautifulSoup
 import requests
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-import pprint
 
 
 dotenv_path = join(dirname(__file__), '.env')
@@ -20,25 +19,26 @@ user_date = input(
     "What date would you like the top 100 songs for? (YYYY-MM-DD): ")
 response = requests.get("https://billboard.com/charts/hot-100/" + user_date)
 soup = BeautifulSoup(response.text, "html.parser")
-
 artist_list = [title.getText().strip("\n").replace(" Featuring", ",").replace(" &", ",")
                for title in soup.select("span.c-label.a-no-trucate")]
-
 titles_list = [title.getText().strip("\n")
                for title in soup.select("li h3")][:100]
 
-
 scope = "playlist-modify-private"
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-    scope=scope, redirect_uri=SPOTIPY_REDIRECT_URI, client_id=CLIENT_ID, client_secret=CLIENT_SECRET))
+    scope=scope,
+    redirect_uri=SPOTIPY_REDIRECT_URI,
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    show_dialog=True,
+    cache_path="token.txt"
+))
 
-year = user_date.split("-")[0]
 song_urls = []
-
 for i in range(0, 100):
     track_info = sp.search(
-        q=f"{titles_list[i]} {artist_list[i]}", type="track")
-
+        q=f"{titles_list[i]} {artist_list[i]}",
+        type="track")
     try:
         url = track_info["tracks"]["items"][0]["uri"].split(":")[2]
         song_urls.append(url)
@@ -46,5 +46,11 @@ for i in range(0, 100):
         print(
             f"{titles_list[i]} by {artist_list[i]} does not exist on Spotify. Skipped.")
 
-print(song_urls)
-print(len(song_urls))
+playlist = sp.user_playlist_create(
+    user=sp.current_user()["id"],
+    name=f"{user_date} Billboard 100",
+    description=f"Billboard top 100 from the date: {user_date}.",
+    public=False
+)
+
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_urls)
